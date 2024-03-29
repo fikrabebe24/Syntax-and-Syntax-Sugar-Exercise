8b0ef42dbbae5e32a83a062b18ecfde4494bb8d7
Fikr Abebe <fikrabebe24@gmail.com>
2024-03-29 12:38:49 -0400

    Syntax and Sugar Exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+
+            //int answer = 4 (explicit typing)
+            //string response; //(explicit typing)
+            //if (answer < 9)
+            //{
+            //response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            // response = answer + " is greater than or equal to nine";
+            //}
+
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+
+
         }
     }
 }
